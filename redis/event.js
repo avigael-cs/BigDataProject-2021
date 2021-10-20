0574cc0be73f0ecf6798a7ae92cc14bb48128761
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2021-10-20 12:11:35 +0300

    event

---
+++ redis/event.js
@@ -0,0 +1,60 @@
+
+module.exports.EventObj = class EventObj{
+    constructor(id_package, items, size, price, adress, day, time, destination){
+        
+        this.id_package = id_package;
+        this.items = items;
+        this.size = size;
+        this.price = price;
+        this.adress = adress;
+        this.day = day;
+        this.time = time;
+        this.destination = destination;
+    }
+
+    getCarNumber(){
+        return this.id_package;
+    }
+    getEventKind(){
+        return this.items;
+    }
+
+    getSection(){
+        return this.size;
+    }
+
+    getDirection(){
+        return this.price;
+    }
+
+    getCarKind(){
+        return this.adress;
+    }
+
+    getDay(){
+        return this.day;
+    }
+
+    getTime(){
+        return this.time;
+    }
+
+    getIsSpecialDay(){
+        return this.destination;
+    }
+
+    setEventKind(items){
+        this.items = items;
+    }
+
+    setSection(size){
+        this.size = size;
+    }
+    setTime(time){
+        this.time = time;
+    }
+
+    toString(){
+        return `{"id_package":"${this.id_package}","items":"${this.items}","size":"${this.size}","price":"${this.price}","adress":"${this.adress}","day":"${this.day}","time":"${this.time}","destination":"${this.destination}"}`;
+    }
+}
