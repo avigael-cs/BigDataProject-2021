1cc113ad8340bfdb9c8908e9216e8c2e208ee6a1
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2021-10-25 13:57:23 +0300

    bigml

--- redis/redisreceiver.js
+++ redis/redisreceiver.js
@@ -5,9 +5,17 @@ var server = require('http').Server(app);
 var redis = require('redis');
 const { myPrediction } = require('./bigML');
 var redisClient = redis.createClient();
-var sub = redis.createClient()
+var sub = redis.createClient()/// subscribe ("message")
+// publish("message")
 redisClient.subscribe('message');
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+let all_the_packages=new Map();
+var packages=[]
+var total_packages_number = 0 ;
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -15,6 +23,7 @@ app.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
+
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
@@ -24,41 +33,15 @@ app.use(function (err, req, res, next) {
     });
 });
 
+redisClient.publish("message",()=>{
 
-redisClient.on("message", function (channel, data) {
-    console.log("Got the Massage", data);
-    var data = JSON.parse(data, function (key, value) {
+})
+redisClient.subscribe("package")
+redisClient.on("message",(channel,message)=>{
+    // on send
 
-        if (key == 'enter_section') {
-            numOfpackages[value]++
-            numOfpackages[0]++
-            switch (value) 
-            {}
-        }
-        // exit packages
-        listOfDetails = [package_details1, package_details2, package_details3, package_details4, package_details5]
-        for (var i = 0; i < 5; i++) {
-            // exit(listOfDetails[i], i)
-            for (var j = 0; j < listOfDetails[i].length; j++) {
-                var myRandom = Math.random()
-                if (myRandom < 0.0005) {
-                    listOfDetails[i].splice(j, 1)
-                    numOfpackages[0]--;
-                    numOfpackages[i + 1]--;
-                    totalexitpackages++;
-                }
-            }
-        }
-        module.exports.totalExitpackages = totalexitpackages;
-        module.exports.packages = numOfpackages;
-        module.exports.package_Gdetails1 = package_details1;
-        module.exports.package_Gdetails2 = package_details2;
-        module.exports.package_Gdetails3 = package_details3;
-        module.exports.package_Gdetails4 = package_details4;
-        module.exports.package_Gdetails5 = package_details5;
+})
 
-    });
-});
 
 redisClient.on('connect', function () {
     console.log('Reciver connected to Redis');
