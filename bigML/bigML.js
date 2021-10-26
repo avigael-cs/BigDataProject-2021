ac09f43cf789fda4565836294a0300e39ae8f61f
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2021-10-26 12:41:18 +0300

    bigML

--- bigML/bigML.js
+++ bigML/bigML.js
@@ -11,11 +11,52 @@ const connectionBigml = new bigml.BigML('AfikPeretz', '143dcb7fa89214cb41e233c41
 const bigmlModel = new bigml.Model(connectionBigml)
 
 
+var matrix = [];
+for(var i=0; i<5; i++) {
+    matrix[i] = [];
+    for(var j=0; j<5; j++) {
+        matrix[i][j] = 0;
+    }
+}
 
-        catch (err) {
-            console.error(err)
-        }
+const mongo=require('../Mongo/mongo');
+
+// var source = new bigml.Source(connection);
+// source.create("./csv_bigml.csv", function(error, sourceInfo) {
+//   if (!error && sourceInfo) {
+//     var dataset = new bigml.Dataset();
+//     dataset.create(sourceInfo, function(error, datasetInfo) {
+//       if (!error && datasetInfo) {
+//         var model = new bigml.Model();
+//         model.create(datasetInfo, function (error, modelInfo) {
+//           if (!error && modelInfo) {
+//             var prediction = new bigml.Prediction();
+//             prediction.create(modelInfo, {"": ""})
+
+//           }
+//         });
+//       }
+//     });
+//   }
+// });
+
+
+
+ 
+
+module.exports.get_predict=(event) =>{
+var localModel = new bigml.LocalModel('model/61085252c1c0000b93094151',connection);
+localModel.predict({"id":event.id,"items":event.items,"price":event.price,"size":event.size,"dest":event.dest,"from":event.from },
+function(error, prediction) {
+    // console.log(prediction.prediction)
+   var  pred = parseInt(prediction.prediction)
+   console.log(pred)
+
+   matrix[pred-1][event.Section-1]++
     
+});
 
-module.exports = BigMLHandler
-module.exports.startUpdating = startUpdating
+module.exports.Matrix=matrix;
+
+}
+
