663ff9bbcb3e85a3bf35f2ecda9ee4522787a143
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2021-10-27 14:36:11 +0300

    connection

--- dashboard-server/db/index.js
+++ dashboard-server/db/index.js
@@ -1,3 +1,6 @@
+
+//bigdata2021s@gmail.com
+// connect https://console.firebase.google.com/u/0/project/packagemanager-214c6/firestore/data/~2Fpackages~2F08d6a36d-b89b-497a-9692-640d3f5bd2fa
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
